f7d2d9eed6776d9515cb595e096ea025f7dee784
kan <1736132+kankichi77@users.noreply.github.com>
2022-07-08 10:02:52 +0800

    update

--- hibor.py
+++ hibor.py
@@ -1,15 +1,25 @@
-import urllib.request
-import json
-import datetime
+import urllib.request, json, datetime
 
-today_YYMMDD = datetime.datetime.now().strftime('%y%m%d')
-filename = 'hibor_' + today_YYMMDD + '.txt'
-f = open(filename, "w")
-# url = 'https://api.hkma.gov.hk/public/market-data-and-statistics/monthly-statistical-bulletin/er-ir/hk-interbank-ir-daily?segment=hibor.fixing&offset=0&pagesize=10'
-url = 'https://api.hkma.gov.hk/public/market-data-and-statistics/daily-monetary-statistics/daily-figures-interbank-liquidity?fields=end_of_date,hibor_fixing_1m&pagesize=1'
-r = urllib.request.urlopen(url).read().decode('ascii')
-j = json.loads(r)
-f.write(json.dumps(j,indent=2))
-f.close()
-print('Date: {0}'.format(j["result"]["records"][0]["end_of_date"]))
-print('1-Month HIBOR Fixing: {0}%'.format(j["result"]["records"][0]["hibor_fixing_1m"]))
+  BASE_API_URL = 'https://api.hkma.gov.hk/public/market-data-and-statistics/daily-monetary-statistics/daily-figures-interbank-liquidity'
+
+def printHiborDataFromApi():
+  datefrom, dateto, recordcount = 0, 0, 0
+  #datefrom = '2021-11-01'
+  #dateto = '2021-11-30'
+  recordcount = 10
+
+  url = BASE_API_URL + '?offset=0&fields=end_of_date,hibor_fixing_1m'
+  if datefrom and dateto:
+    url += f'&choose=end_of_date&from={datefrom}&to={dateto}'
+  j = json.loads(urllib.request.urlopen(url).read())
+  l = j["result"]["records"]
+  if recordcount: l = l[0:recordcount]
+  l.reverse()
+
+  for r in l:
+    print(f"{r['end_of_date']}\t{r['hibor_fixing_1m']}%")
+    #print(f"{r['hibor_fixing_1m']}%")
+
+if __name__ == "__main__":
+  printHiborDataFromApi()
+
